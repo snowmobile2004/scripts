f233e16862bcc98319636568924298f5d390a610
snowmobile2004 <greenjosh6499@gmail.com>
2021-03-02 23:19:46 -0500

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -5,3 +5,5 @@ bash ./install.sh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
 curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zshrc > ~/.zshrc
 curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/fishy.zsh-theme | tee ~/.oh-my-zsh/themes/fishy.zsh-theme
+curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zsh_aliases > ~/.zsh_aliases
+curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zshrc > ~/.zshrc
