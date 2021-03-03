553fdffdadf5f29267c42ba7b3354e6653b1cf48
snowmobile2004 <greenjosh6499@gmail.com>
2021-03-02 23:26:09 -0500

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -2,7 +2,7 @@ sudo apt install -y zsh
 curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh > install.sh
 chmod +x install.sh
 bash ./install.sh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
 curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zshrc > ~/.zshrc
 curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/fishy.zsh-theme | tee ~/.oh-my-zsh/themes/fishy.zsh-theme
 curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zsh_aliases > ~/.zsh_aliases
