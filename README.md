d0c90a6e986746663dc53c52c9e3d5008e9d7707
snowmobile2004 <greenjosh6499@gmail.com>
2021-05-20 10:57:43 -0400

    changed kickstart to not install docker

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ To install ZSH shell on a machine, run:
 
 ```
 sudo sh -c "$(curl -fsSL https://raw.github.com/snowmobile2004/scripts/master/kickstart.sh)"
+sudo sh -c "$(curl -fsSL https://raw.github.com/snowmobile2004/scripts/master/install-docker.sh)"
 ```
--- kickstart.sh
+++ kickstart.sh
@@ -12,9 +12,6 @@ curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zsh_alias
 mkdir ~/bin
 curl https://raw.githubusercontent.com/snowmobile2004/scripts/master/z.sh > ~/bin/z.sh
 echo 'Acquire { HTTP::proxy "http://10.0.5.20:3142"; HTTPS::proxy "http://10.0.5.20:3142"; }' | tee /etc/apt/apt.conf.d/proxy
-echo Initial Kickstart Completed
-curl -fsSL https://raw.githubusercontent.com/snowmobile2004/scripts/master/install-docker.sh -o install-docker.sh
-chmod +x install-docker.sh
-bash ./install-docker.sh
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmBe/xpoI6waS6VtOCUGi6pJsJ3zcljvsMjo+ti8uJraRNarAaJpgCpmfwmSQRb6BIDSxIZxLUFSgQzS5+IMJf1pDIcXwFcg3XXDlnkdL6lBUurwCxx5WQcsOw3CLNjpcq9zzH6NJ7klSdR0KyrWb1Dt3EiPjORbGmkLLZ7Ca3wAh38q5KsUlQ63mRYS75tF+ZjtpO6ZmYJ0+yWNfqJrDn/S7makB0ZaCiOd+Szr037LVIWVX5DayGFqZ/mcaWP1nYKMqv+03YB2e+QguzPjC4C5L3bVpynYKFskb+6H1sKFTNjeagxlZuszJLUZTcGJ3KWVB6pylW4/j4006jZBLpw== rsa-key-20210213' \ tee .ssh/"authorized keys"
+echo Initial Kickstart Completed
 exit
