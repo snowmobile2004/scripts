87d982b6d16aeb45bfb3b24f5df58fb2f49c10f1
snowmobile2004 <greenjosh6499@gmail.com>
2021-07-14 15:29:22 -0400

    Update kickstart.sh

--- kickstart.sh
+++ kickstart.sh
@@ -2,11 +2,11 @@
 sudo apt update
 sudo apt upgrade -y && sudo apt install -y zsh
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-chmod +x install.sh
+sudo chmod +x install.sh
 ZSH="/home/snow/.oh-my-zsh" sh install.sh
-bash ./home/snow/install.sh --unattended
+bash install.sh --unattended
 chsh -s /bin/zsh snow
-chsh -s /bin/zsh root
+sudo chsh -s /bin/zsh root
 git clone https://github.com/snowmobile2004/bin.git | ~/bin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
 curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zshrc > ~/.zshrc
@@ -14,12 +14,11 @@ curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/fishy.zsh-
 curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zsh_aliases > ~/.zsh_aliases
 curl https://raw.githubusercontent.com/snowmobile2004/scripts/master/z.sh > ~/z.sh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/snow/zsh-syntax-highlighting
-curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zshrc > /home/snow/.zshrc
+curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zshrc > ~/.zshrc
 curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/fishy.zsh-theme | tee /home/snow/.oh-my-zsh/themes/fishy.zsh-theme
-curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zsh_aliases > /home/snow/.zsh_aliases
-curl https://raw.githubusercontent.com/snowmobile2004/scripts/master/z.sh > /home/snow/z.sh
+curl https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zsh_aliases > ~/.zsh_aliases
+curl https://raw.githubusercontent.com/snowmobile2004/scripts/master/z.sh > ~/z.sh
 echo 'Acquire { HTTP::proxy "http://10.0.5.20:3142"; HTTPS::proxy "http://10.0.5.20:3142"; }' | tee /etc/apt/apt.conf.d/proxy
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmBe/xpoI6waS6VtOCUGi6pJsJ3zcljvsMjo+ti8uJraRNarAaJpgCpmfwmSQRb6BIDSxIZxLUFSgQzS5+IMJf1pDIcXwFcg3XXDlnkdL6lBUurwCxx5WQcsOw3CLNjpcq9zzH6NJ7klSdR0KyrWb1Dt3EiPjORbGmkLLZ7Ca3wAh38q5KsUlQ63mRYS75tF+ZjtpO6ZmYJ0+yWNfqJrDn/S7makB0ZaCiOd+Szr037LVIWVX5DayGFqZ/mcaWP1nYKMqv+03YB2e+QguzPjC4C5L3bVpynYKFskb+6H1sKFTNjeagxlZuszJLUZTcGJ3KWVB6pylW4/j4006jZBLpw== rsa-key-20210213' \ tee .ssh/"authorized keys"
-rm /home/snow/install.sh
 echo Initial Kickstart Completed
 exit
