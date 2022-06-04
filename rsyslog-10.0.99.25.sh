86e43e6ee425e442a3000a438069cfccc6219917
snowmobile2004 <greenjosh6499@gmail.com>
2022-06-04 00:14:51 -0400

    Changed line ending to LF

--- rsyslog-10.0.99.25.sh
+++ rsyslog-10.0.99.25.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt update
 sudo apt install rsyslog
-sudo echo "*.* @10.0.99.25;RSYSLOG_SyslogProtocol23Format" >> /etc/rsyslog.d/90-gravwell.conf
+echo "*.* @10.0.99.25;RSYSLOG_SyslogProtocol23Format" | sudo tee /etc/rsyslog.d/90-gravwell.conf > /dev/null
 echo rSyslog logging to 10.0.99.25 has been configured.
 exit
