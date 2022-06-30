5dc7eb7ade4e4cf9562b32c9f720016ccd370ad7
snowmobile2004 <greenjosh6499@gmail.com>
2022-06-30 11:49:18 -0400

    added update-portainer.sh rev1

---
+++ update-portainer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker stop portainer
+docker rm portainer
+docker pull portainer/portainer-ce:latest
+docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker service update --image portainer/agent:latest --force portainer_agent 
+echo Portainer has been updated successfully.
