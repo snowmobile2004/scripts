8f06642565459517ae35ad86fb2600f2326c2e70
snowmobile2004 <greenjosh6499@gmail.com>
2022-06-30 11:50:19 -0400

    fixed portainer agent service name

--- update-portainer.sh
+++ update-portainer.sh
@@ -3,5 +3,5 @@ docker stop portainer
 docker rm portainer
 docker pull portainer/portainer-ce:latest
 docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-docker service update --image portainer/agent:latest --force portainer_agent 
+docker service update --image portainer/agent:latest --force portainer-agent_agent 
 echo Portainer has been updated successfully.
