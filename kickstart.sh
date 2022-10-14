8b3adb8c551f6a5f59ba3e9cd3112e41f1d1298d
snow <greenjosh6499@gmail.com>
2022-10-14 01:35:25 -0400

    added prompt for APT proxy

--- kickstart.sh
+++ kickstart.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+IFS= read -r -p "Setup APT Proxy? <y/n> " prompt
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+then
+msg_info "Adding APT proxy to /etc/apt.conf.d"
+echo 'Acquire { HTTP::proxy "http://10.0.5.20:3142"; HTTPS::proxy "http://10.0.5.20:3142"; }' | sudo tee /etc/apt/apt.conf.d/proxy
+msg_ok "Added APT proxy to /etc/apt.conf.d"
+fi
 sudo apt update
-sudo apt upgrade -y && sudo apt install -y zsh direnv
+sudo apt upgrade -y && sudo apt install -y zsh direnv unzip
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/snowmobile2004/bin.git | ~/bin
 sudo chsh -s /bin/zsh snow
@@ -11,7 +18,6 @@ curl -fsSL https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/fish
 curl -fsSL https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zsh_aliases > ~/.zsh_aliases
 curl -fsSL https://raw.githubusercontent.com/snowmobile2004/scripts/master/z.sh > ~/bin/z.sh
 curl -fsSL https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zsh_aliases > ~/.zsh_aliases
-echo 'Acquire { HTTP::proxy "http://10.0.5.20:3142"; HTTPS::proxy "http://10.0.5.20:3142"; }' | sudo tee /etc/apt/apt.conf.d/proxy
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmBe/xpoI6waS6VtOCUGi6pJsJ3zcljvsMjo+ti8uJraRNarAaJpgCpmfwmSQRb6BIDSxIZxLUFSgQzS5+IMJf1pDIcXwFcg3XXDlnkdL6lBUurwCxx5WQcsOw3CLNjpcq9zzH6NJ7klSdR0KyrWb1Dt3EiPjORbGmkLLZ7Ca3wAh38q5KsUlQ63mRYS75tF+ZjtpO6ZmYJ0+yWNfqJrDn/S7makB0ZaCiOd+Szr037LVIWVX5DayGFqZ/mcaWP1nYKMqv+03YB2e+QguzPjC4C5L3bVpynYKFskb+6H1sKFTNjeagxlZuszJLUZTcGJ3KWVB6pylW4/j4006jZBLpw== rsa-key-20210213' \ sudo tee .ssh/"authorized keys"
 git config --global user.name "Josh Green"
 git config --global user.email "greenjosh6499@gmail.com"
