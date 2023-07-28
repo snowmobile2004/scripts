868155b5c606c5c110640ee30766cc7e7aec16ca
snow <greenjosh6499@gmail.com>
2023-07-28 01:22:02 -0400

    added yadm clone

--- kickstart-cloud-init.sh
+++ kickstart-cloud-init.sh
@@ -15,10 +15,10 @@ sudo chsh -s /bin/zsh root
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
 
-curl -fsSL https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zshrc > ~/.zshrc
-curl -fsSL https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/fishy.zsh-theme | sudo tee ~/.oh-my-zsh/themes/fishy.zsh-theme
+curl -fsSL https://raw.githubusercontent.com/snowmobile2004/scripts/master/zsh-theme/fishy.zsh-theme | sudo tee ~/.oh-my-zsh/themes/fishy.zsh-theme
 curl -fsSL https://raw.githubusercontent.com/snowmobile2004/scripts/master/z.sh > ~/bin/z.sh
-curl -fsSL https://raw.githubusercontent.com/snowmobile2004/dotfiles/master/.zsh_aliases > ~/.zsh_aliases
+
+yadm clone https://github.com/snowmobile2004/dotfiles.git
 
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmBe/xpoI6waS6VtOCUGi6pJsJ3zcljvsMjo+ti8uJraRNarAaJpgCpmfwmSQRb6BIDSxIZxLUFSgQzS5+IMJf1pDIcXwFcg3XXDlnkdL6lBUurwCxx5WQcsOw3CLNjpcq9zzH6NJ7klSdR0KyrWb1Dt3EiPjORbGmkLLZ7Ca3wAh38q5KsUlQ63mRYS75tF+ZjtpO6ZmYJ0+yWNfqJrDn/S7makB0ZaCiOd+Szr037LVIWVX5DayGFqZ/mcaWP1nYKMqv+03YB2e+QguzPjC4C5L3bVpynYKFskb+6H1sKFTNjeagxlZuszJLUZTcGJ3KWVB6pylW4/j4006jZBLpw== rsa-key-20210213' | sudo tee -a ~/.ssh/authorized_keys
 
